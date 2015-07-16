b9de2484a081ceaa1c1ec6ed5379a51a9ad1d2a0
Willdogx <alvarezsantiago96@gmail.com>
2015-07-16 05:15:02 -0300

    Lesson2

--- app/src/main/java/com/example/santi/lesson2/MainActivity.java
+++ app/src/main/java/com/example/santi/lesson2/MainActivity.java
@@ -1,38 +1,48 @@
 package com.example.santi.lesson2;
 
-import android.support.v7.app.ActionBarActivity;
+
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.RelativeLayout;
 
+import java.util.Random;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    Chronometer focus;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        focus = (Chronometer) findViewById(R.id.chronometer1);
+        final String[] values = getResources().getStringArray(R.array.colors_array);
+        final RelativeLayout root = (RelativeLayout) findViewById(R.id.main_layout);
+        root.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String[] colors = {"#ff0000","#ffa500","#000000"};
+                int idx = new Random().nextInt(colors.length);
+                String random = (colors[idx]);
+                View layout = findViewById( R.id.main_layout );
+                layout.setBackgroundColor(R.color.black);
+
+                focus.setBase(SystemClock.elapsedRealtime());
+                focus.start();
+            }
+        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
