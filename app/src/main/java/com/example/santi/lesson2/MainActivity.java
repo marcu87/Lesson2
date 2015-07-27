bd2ea923262dabed60a32c78243566ebf61625af
Willdogx <alvarezsantiago96@gmail.com>
2015-07-27 05:38:13 -0300

    Lesson2

--- app/src/main/java/com/example/santi/lesson2/MainActivity.java
+++ app/src/main/java/com/example/santi/lesson2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.santi.lesson2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.view.Menu;
 import android.view.View;
@@ -16,33 +17,66 @@ public class MainActivity extends Activity {
 
     Chronometer focus;
     private TextView myText = null;
+    int b1,b2;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView myTextView = (TextView) findViewById(R.id.MyTextView);
+        final TextView myTextView = (TextView) findViewById(R.id.MyTextView);
         myTextView.setVisibility(View.GONE);
         focus = (Chronometer) findViewById(R.id.chronometer1);
         final String[] values = getResources().getStringArray(R.array.colors_array);
         final RelativeLayout root = (RelativeLayout) findViewById(R.id.main_layout);
+        b1=0;
+        b2=0;
+        final int black = getResources().getColor(R.color.black);
         root.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                int[] androidColors = getResources().getIntArray(R.array.androidcolors);
-                int randomAndroidColor = androidColors[new Random().nextInt(androidColors.length)];
-                root.setBackgroundColor(randomAndroidColor);
+                Handler h = new Handler();
+                Runnable r = null;
 
-                focus.setBase(SystemClock.elapsedRealtime());
-                focus.start();
-
-                int black = getResources().getColor(R.color.black);
-                if(randomAndroidColor==black){
+                if (b1 == 1) {
+                    b2=1;
                     focus.setVisibility(View.GONE);
-                    TextView myTextView = (TextView) findViewById(R.id.MyTextView);
                     myTextView.setVisibility(View.VISIBLE);
+
+                } else {
+                    int[] androidColors = getResources().getIntArray(R.array.androidcolors);
+                    int randomAndroidColor = androidColors[new Random().nextInt(androidColors.length)];
+                    root.setBackgroundColor(randomAndroidColor);
+
+                    focus.setBase(SystemClock.elapsedRealtime());
+                    focus.start();
+
+                    if (randomAndroidColor == black) {
+                        b1 = 1;
+                        h = new Handler();
+
+                        final Handler finalH = h;
+                        r = new Runnable() {
+
+                            public void run() {
+                                int[] androidColors = getResources().getIntArray(R.array.androidcolors);
+                                int randomAndroidColor = androidColors[new Random().nextInt(androidColors.length)];
+                                root.setBackgroundColor(randomAndroidColor);
+                                b1 = 0;
+                                if(b2==1){
+                                  finalH.removeCallbacksAndMessages(null);
+                                }
+                            }
+
+                        };
+                        h.postDelayed(r, 3000);
+
+                    }
+
+
+
                 }
+
             }
 
         });
